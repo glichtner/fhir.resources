cc24359c1afb997355e6240811c17730dbe1e3ba
Gregor Lichtner <gregor.lichtner@med.uni-greifswald.de>
2023-07-03 12:12:40 +0200

    restricting pydantic to <2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-requirements = ["pydantic[email]>=1.7.2"]
+requirements = ["pydantic[email]>=1.7.2,<2.0"]
 
 setup_requirements = ["pytest-runner"]
 
